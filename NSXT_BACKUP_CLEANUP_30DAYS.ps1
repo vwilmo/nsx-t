b2c969f32336a6c298b18ce239f78d4c19ed7465
vwilmo <33289176+vwilmo@users.noreply.github.com>
2019-09-03 16:58:21 -0500

    Add files via upload

---
+++ NSXT_BACKUP_CLEANUP_30DAYS.ps1
@@ -0,0 +1,7 @@
+﻿# Delete all Files in C:\temp older than 30 day(s)
+$Path = "C:\ftproot\nsxt"
+$Daysback = "-30"
+ 
+$CurrentDate = Get-Date
+$DatetoDelete = $CurrentDate.AddDays($Daysback)
+Get-ChildItem $Path -Recurse -include *.json, *.tar, backup* | Where-Object { $_.LastWriteTime -lt $DatetoDelete } | Remove-Item -Recurse
